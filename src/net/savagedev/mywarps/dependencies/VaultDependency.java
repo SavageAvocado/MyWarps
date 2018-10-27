403fded0ca763db2242b3f9f50ef929b8c526faf
David Acevedo <acevedo72799@gmail.com>
2018-10-27 19:34:35 -0400

    Initial commit.

--- src/net/savagedev/mywarps/dependencies/VaultDependency.java
+++ src/net/savagedev/mywarps/dependencies/VaultDependency.java
@@ -18,23 +18,18 @@ public class VaultDependency extends Dependency<Vault> {
     @Override
     public void onHook() {
         RegisteredServiceProvider<Economy> rsp;
-        if ((rsp = this.getHandlingPlugin().getServer().getServicesManager().getRegistration(Economy.class)) == null) {
+        if ((rsp = this.getHandlingPlugin().getServer().getServicesManager().getRegistration(Economy.class)) == null)
             this.getHandlingPlugin().getPluginManager().disablePlugin(this.getHandlingPlugin());
-        }
 
-        if ((this.economy = rsp.getProvider()) == null) {
+        if ((this.economy = rsp.getProvider()) == null)
             this.getHandlingPlugin().getPluginManager().disablePlugin(this.getHandlingPlugin());
-        }
     }
 
     public void performTransaction(Player user, double amount) {
-        if (amount < 0.0) {
+        if (amount < 0.0D)
             this.getEconomy().depositPlayer(user, amount);
-            Bukkit.getServer().getLogger().info( amount + " less than 0.0. Paying player " + Math.abs(amount));
-        } else {
+        else
             this.getEconomy().withdrawPlayer(user, amount);
-            Bukkit.getServer().getLogger().info(amount + " more than 0.0. Withdrawing " + amount + " from player.");
-        }
     }
 
     public Economy getEconomy() {
